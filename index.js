0912e3c9b5b2cbe8bd1ecef94c66faf3ea6e09e9
Md. Dilshad Alam <mddalam1@gmail.com>
2021-10-26 11:01:41 +0530

    Implemented Data Fetching from a Target Website.

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const puppeteer = require("puppeteer");
     await page.goto("https://learnwebcode.github.io/practice-requests/");
     // await page.screenshot({ path: "amazing.png", fullPage: true });
 
-    const names = ["text1", "text2", "text3"];
+    const names = await page.evaluate(() => {
+      // Inside this anonymous function we are dealing with the client side.
+      // We can run all JavaScript Functions that we can run inside DOM.
+      // All console.logs will console the data in browser and not in Terminal.
+      // The variables will also be not accessible outside this scope.
+      return Array.from(
+        document.querySelectorAll("body > div > div > div > strong")
+      ).map((x) => x.textContent);
+    });
     await fs.writeFile("names.txt", names.join("\r\n"));
 
     await browser.close();
