1a7be77ced14573a9ac3e4412b761b3a2ed87268
Md. Dilshad Alam <mddalam1@gmail.com>
2021-10-26 10:38:36 +0530

    Taking Screenshot of Entire Page.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require("puppeteer");
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 720 });
     await page.goto("https://github.com/puppeteer/puppeteer");
-    await page.screenshot({ path: "amazing.png" });
+    await page.screenshot({ path: "amazing.png", fullPage: true });
     await browser.close();
   } catch (error) {
     console.log(error);
