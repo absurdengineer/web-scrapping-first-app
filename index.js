5c6405550b8e9ca77de3e4332bdab49dabc850e0
Md. Dilshad Alam <mddalam1@gmail.com>
2021-10-26 11:57:02 +0530

    Performed Click Event on Targetted site and Fetched some data based on the event.

--- index.js
+++ index.js
@@ -17,17 +17,24 @@ const puppeteer = require("puppeteer");
     //     document.querySelectorAll("body > div > div > div > strong")
     //   ).map((x) => x.textContent);
     // });
-    const photos = await page.$$eval("img", (images) => {
-      // $$eval() is a function provided by the puppeteer which is specially dsigned to
-      // deal with multiple elements with the same selector that basically takes 2 argument.
-      // 1 the css selector, and 2 is an arrow function that takes 1 argument which is an array of
-      // the specified element which is passed by the $$eval() only.
-      return images.map((image) => image.src);
-    });
-    for (const photo of photos) {
-      const imagePage = await page.goto(photo);
-      await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
-    }
+    // const photos = await page.$$eval("img", (images) => {
+    //   // $$eval() is a function provided by the puppeteer which is specially dsigned to
+    //   // deal with multiple elements with the same selector that basically takes 2 argument.
+    //   // 1 the css selector, and 2 is an arrow function that takes 1 argument which is an array of
+    //   // the specified element which is passed by the $$eval() only.
+    //   return images.map((image) => image.src);
+    // });
+    // for (const photo of photos) {
+    //   const imagePage = await page.goto(photo);
+    //   await fs.writeFile(photo.split("/").pop(), await imagePage.buffer());
+    // }
+    await page.click("#clickme");
+    const data = await page.$eval("#data", (data) => data.textContent);
+    // $eval() is a function provided by the puppeteer which is specially dsigned to
+    // deal with single elements with the same selector that basically takes 2 argument.
+    // 1 the css selector, and 2 is an arrow function that takes 1 argument which is
+    // the specified element which is passed by the $eval() only.
+    console.log(data);
     await browser.close();
   } catch (error) {
     console.log(error);
