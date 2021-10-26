fbd27c7c2e56e0b7b844c1f603c47fcf8d7d13a5
Md. Dilshad Alam <mddalam1@gmail.com>
2021-10-26 10:37:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 720 });
+    await page.goto("https://github.com/puppeteer/puppeteer");
+    await page.screenshot({ path: "amazing.png" });
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+})();
